99e7cfb712f35c0e1324a424a60766ebb881e679
Anwesha Naskar <anweshan@anweshan-macbookpro.roam.corp.google.com>
2020-05-05 16:26:21 -0700

    Adds static version of spreadsheet add on to to-gatsby folder

--- to-gatsby/src/pages/spreadsheet-add-on/index.tsx
+++ to-gatsby/src/pages/spreadsheet-add-on/index.tsx
@@ -13,10 +13,34 @@
 // limitations under the License.
 
 import * as React from "react"
+import Typography from "@material-ui/core/Typography"
 
 import Layout from "../../components/layout"
 
 const SpreadsheetAddOn = () => {
-  return <Layout title="Spreadsheet Add-on">TODO - add in content.</Layout>
+  return <Layout title="Spreadsheet Add-on">
+    <Typography variant="h2">Overview</Typography>
+
+    <Typography variant="body1">
+    <p>The <a href="https://developers.google.com/analytics/solutions/google-analytics-spreadsheet-add-on">Google Analytics Spreadsheet Add-on</a> makes it easier for Google Analytics users to access, visualize, share, and manipulate their data in Google Spreadsheets. The add-on gives you the full power of the Google Analytics <a href="https://developers.google.com/analytics/devguides/reporting/">Core and Multi-Channel Funnels Reporting APIs</a> without requiring you to know or write any code.</p>
+
+    <p>With this tool, you can:</p>
+
+      <ul>
+        <li>Query data from multiple views.</li>
+        <li>Create custom calculations from your report data.</li>
+        <li>Create visualizations with the built-in visualization tools, and embed those visualizations on third-party websites.</li>
+        <li>Schedule your reports to run and update automatically.</li>
+        <li>Easily control who can see your data and visualizations by leveraging Google Spreadsheet's existing sharing and privacy features.</li>
+      </ul>
+      </Typography>
+      <Typography variant="h2">Learn More</Typography>
+
+      <Typography variant="body1">
+      <p>To start using the Google Analytics Spreadsheet add-on you can simply download it from the <a href="{{ project.external.url }}">Chrome Web Store</a> and try adding it to one of your spreadsheets today.</p>
+
+      <p>If you have questions, check out the <a href="https://developers.google.com/analytics/solutions/google-analytics-spreadsheet-add-on">solution guide</a> which explains how to install and use the add-on and goes over each option in detail.</p>
+      </Typography>
+  </Layout>
 }
 export default SpreadsheetAddOn
